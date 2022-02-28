faa97e66b4d7feb24a458d0c233761ec8f039806
topherlee <topher.lee10@gmail.com>
2022-02-28 16:43:57 +0000

    finished creating database from csv files

---
+++ parse_csv.py
@@ -0,0 +1,50 @@
+import csv
+import sqlite3
+
+#creates connection to SQLite Database
+connection = sqlite3.connect("artist_tracklist.db")
+cursor = connection.cursor()
+
+#prevent duplicates before creating table
+cursor.execute('DROP TABLE IF EXISTS artists')
+cursor.execute('DROP TABLE IF EXISTS tracklists')
+cursor.execute('CREATE TABLE artists (artist_id TEXT, artist_name TEXT, artist_mbid TEXT)')
+cursor.execute('CREATE TABLE tracklists (artist_id TEXT, track_id TEXT, artist_name TEXT, track_name TEXT, year INTEGER, FOREIGN KEY (artist_name) REFERENCES artists(artist_name), FOREIGN KEY (artist_id) REFERENCES artists(artist_id))')
+
+#reading from artist csv file
+with open('dataset/unique_artists.csv', newline='') as artist_file:
+    reader = csv.reader(artist_file, delimiter=",")
+    next(reader)                                    #skip header line
+    print("Processing artists names")
+    for row in reader:
+        #print(row)
+        artist_id = row[0]
+        artist_mbid = row[1]
+        track_id = row[2]
+        artist_name = row[3]
+        cursor.execute('INSERT INTO artists VALUES (?, ?, ?)', (artist_id, artist_name, artist_mbid))
+        connection.commit()
+
+with open('dataset/tracks_2010.csv', newline='') as tracks_file:
+    reader = csv.reader(tracks_file, delimiter=",")
+    next(reader)                                    #skip header line
+    print("Processing track names")
+    for row in reader:
+        #print(row)
+        year = row[0]
+        track_id = row[1]
+        artist_name = row[2]
+        track_name = row[3]
+        
+        #fetch artist_id from artists table
+        cursor.execute('SELECT * FROM artists WHERE artist_name = ?', (artist_name,))
+        ids = cursor.fetchall()
+        #if artist_id for this track cannot be found, skip adding it into the database
+        if ids == []:
+            continue
+        artist_id = ids[0][0]
+
+        cursor.execute('INSERT INTO tracklists VALUES (?, ?, ?, ?, ?)', (artist_id, track_id, artist_name,track_name, year))
+        connection.commit()
+
+connection.close()
