9e2119818c577fdf1c334f63d3a4541196129855
topherlee <topher.lee10@gmail.com>
2022-03-01 00:48:28 +0000

    added search page and hopefully fixed tracks page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os.path, sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 working_dir = os.path.dirname(__file__)
@@ -29,17 +29,48 @@ def artists():
     return render_template('artists.html', title='Artists List', artist_names=artist_names)
     
 
-@app.route('/tracks')
+@app.route('/tracks', methods = ["GET", "POST"])
 def tracks():
+    tracks = None
+    year = None
     cursor = get_db_conn()
-    cursor.execute('SELECT * FROM tracklists ORDER BY track_name')
-    tracks = cursor.fetchall()
+    if request.method == "POST":
+        year = request.form["year"]
+        cursor.execute('SELECT * FROM tracklists WHERE year = ? ORDER BY track_name', (year,))
+        tracks = cursor.fetchall()
+    
     cursor.close()
-    return render_template('tracks.html', title='Tracks List', tracks = tracks)
+    return render_template('tracks.html', title='Tracks List', tracks = tracks, year=year)
 
-@app.route('/search')
+@app.route('/search', methods = ["GET", "POST"])
 def search():
-    pass
+    cursor = get_db_conn()
+    search_type = None
+    query = None
+    artists = None
+    tracks = None
+    statement = ""
+
+    if request.method == "POST":
+        search_type = request.form["search_type"]
+        query_escaped = "%" + request.form["query"] + "%"
+        query = request.form["query"]
+   
+        if search_type == "track":
+            cursor.execute("SELECT * FROM tracklists WHERE track_name LIKE ?", (query_escaped,))
+            tracks = cursor.fetchall()
+            #print error statement if query not found
+            if len(tracks) == 0:
+                statement = f'"{query}" cannot be found'
+        elif search_type == "artist":
+            cursor.execute("SELECT * FROM artists WHERE artist_name LIKE ?", (query_escaped,))
+            artists = cursor.fetchall()
+            #print error statement if query not found
+            if len(artists) == 0:
+                statement = f'"{query}" cannot be found'
+
+    cursor.close()
+    return render_template('search.html', title='Search', query=query, search_type=search_type, artists=artists, tracks=tracks, statement=statement)
 
 @app.route('/artist_details/<id>')
 def artist_details(id):
